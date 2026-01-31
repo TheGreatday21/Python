1a59bad353132088ddd76df88b5a68f2260825a7
TheGreatday21 <kaizzielijah@gmail.com>
2026-01-31 19:38:42 +0300

    Removed the Learning folder from git ignore

---
+++ Learning/My_practice/calling documents.py
@@ -0,0 +1,6 @@
+##opening text
+with open("yee.txt","r") as f:
+    content = f.read()
+    print(content)
+
+
---
+++ Learning/My_practice/modulos.py
@@ -0,0 +1,13 @@
+number = 8
+if number% 2 == 0:
+    print("Even")
+else:
+    print("Un evennumber")
+
+
+
+
+
+
+
+
